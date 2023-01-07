420a446b6b20a9db6ca81670ef8e5754093234c7
shlifedev <shlifedev@gmail.com>
2023-01-07 20:06:57 +0900

    re-write font load logic

--- Sample/Patcher.cs
+++ Sample/Patcher.cs
@@ -17,11 +17,14 @@ public class Patcher
     /// <summary>
     /// 중복 스트링이 있을 수 있으므로 리스트로 관리한다.
     /// </summary>
-    private Dictionary<string, List<UndertaleString>> hashedLocalDatas; 
+    private Dictionary<string, List<UndertaleString>> hashedLocalDatas;
 
     public UndertaleData Data { get; set; }
     public string TranslateFilePath { get; }
     public string FontPath { get; }
+
+    public Dictionary<string, UndertaleTexturePageItem> fontTextureMap = new Dictionary<string, UndertaleTexturePageItem>();
+    public Dictionary<string, JObject> fontYYInfoMap = new Dictionary<string, JObject>();
 
     UndertaleData ReadDataFile(FileInfo datafile)
     {
@@ -76,7 +79,7 @@ public class Patcher
     public Patcher ExportStrings(string savePath)
     {
         List<GameString> strings = new List<GameString>();
-        foreach(var str in Data.Strings)
+        foreach (var str in Data.Strings)
         {
             if (IsMaybePureString(str))
             {
@@ -99,37 +102,37 @@ public class Patcher
         var loadedGameStrings = JsonConvert.DeserializeObject<List<GameString>>(content);
         translateData = new Dictionary<string, GameString>();
         if (loadedGameStrings != null)
-        { 
-            foreach(var str in loadedGameStrings)
+        {
+            foreach (var str in loadedGameStrings)
             {
-                if(str.hash != null && !translateData.ContainsKey(str.hash))
+                if (str.hash != null && !translateData.ContainsKey(str.hash))
                 {
-                    translateData.Add(str.hash, str); 
+                    translateData.Add(str.hash, str);
                 }
             }
         }
-        if(translateData.Count == 0) throw new Exception("번역 데이터 로드실패 (code 2)");
+        if (translateData.Count == 0) throw new Exception("번역 데이터 로드실패 (code 2)");
         if (loadedGameStrings == null) throw new Exception("번역 데이터 로드실패");
 
-        foreach(var internalString in Data.Strings)
+        foreach (var internalString in Data.Strings)
         {
             var hash = CreateMD5(internalString.Content);
             if (!hashedLocalDatas.ContainsKey(hash))
-                hashedLocalDatas.Add(hash, new List<UndertaleString>()); 
+                hashedLocalDatas.Add(hash, new List<UndertaleString>());
             hashedLocalDatas[hash].Add(internalString);
         }
         TranslateFilePath = translateFilePath;
         FontPath = fontPath;
     }
-     
+
 
     public Patcher ApplyTranslate()
     {
-        Console.WriteLine("번역데이터를 게임컨텐츠에 수정중.."); 
-         foreach (var gameString in Data.Strings)
+        Console.WriteLine("번역데이터를 게임컨텐츠에 수정중..");
+        foreach (var gameString in Data.Strings)
         {
             var localHash = CreateMD5(gameString.Content);
-            
+
             if (localHash == "6ADBB6B1451B2167F10AF096AE72DC0C")
             {
                 Console.WriteLine("먀옹");
@@ -137,9 +140,9 @@ public class Patcher
             if (translateData.ContainsKey(localHash))
             {
                 // 게임 실제 패치플로우
-                string content = null;
+                string content = "";
                 content = translateData[localHash].ko;
-                Console.WriteLine("기존" + gameString.Content + "를 " +content  + "로 수정중");
+                Console.WriteLine("기존" + gameString.Content + "를 " + content + "로 수정중");
                 gameString.Content = content;
             }
         }
@@ -147,20 +150,20 @@ public class Patcher
     }
 
 
-    public Patcher ApplyFont()
+    private UndertaleTexturePageItem LoadFontTexture(string fontName)
     {
-        Console.WriteLine("폰트 적용중..");
+        if (fontTextureMap.ContainsKey(fontName)) return fontTextureMap[fontName];
+        // ./localization/font
         var fontDir = new DirectoryInfo(FontPath);
-        var fontYYFiles = fontDir.GetFiles("*.yy").FirstOrDefault();
-        var fontTextureFiles = fontDir.GetFiles("*.png").FirstOrDefault(); 
-
-        Bitmap bitmap = new Bitmap(fontTextureFiles.FullName);
+        var png = new FileInfo(Path.Combine(FontPath, fontName+".png"));
+        // create embeded texture
+        Bitmap bitmap = new Bitmap(png.FullName);
         UndertaleEmbeddedTexture texture = new UndertaleEmbeddedTexture();
         texture.Name = Data.Strings.MakeString("Texture " + Data.EmbeddedTextures.Count); // ???
-        texture.TextureData.TextureBlob = File.ReadAllBytes(fontTextureFiles.FullName);
+        texture.TextureData.TextureBlob = File.ReadAllBytes(png.FullName);
         Data.EmbeddedTextures.Add(texture);
 
-
+        // and register texture page
         UndertaleTexturePageItem texturePageItem = new UndertaleTexturePageItem();
         texturePageItem.Name = Data.Strings.MakeString("PageItem " + Data.TexturePageItems.Count); // ???
         texturePageItem.TexturePage = texture;
@@ -175,94 +178,124 @@ public class Patcher
         texturePageItem.BoundingWidth = (ushort)bitmap.Width;
         texturePageItem.BoundingHeight = (ushort)bitmap.Height;
         Data.TexturePageItems.Add(texturePageItem);
+        fontTextureMap.Add(fontName, texturePageItem);
 
-
-
-        for (int i = 0;  i< Data.Fonts.Count; i++)
+        return texturePageItem;
+    }
+    private JObject LoadFontData(string fontName)
+    {
+        if (fontYYInfoMap.ContainsKey(fontName))
         {
-            JObject fontData = null;
-            var font = Data.Fonts[i]; 
-            var yy = fontYYFiles;
-
-            using (StreamReader file = File.OpenText(yy.FullName))
-            {
-                using (JsonTextReader reader = new JsonTextReader(file))
-                {
-                    fontData = (JObject)JToken.ReadFrom(reader);
-                }
-            }
-             
-             
-            font.Texture = texturePageItem;
-            font.Glyphs.Clear();
-            font.DisplayName = Data.Strings.MakeString((string)fontData["fontName"]);
-            font.EmSize = (uint)fontData["size"];
-            font.Bold = (bool)fontData["bold"];
-            font.Italic = (bool)fontData["italic"];
-            font.Charset = (byte)fontData["charset"];
-            font.AntiAliasing = (byte)fontData["AntiAlias"]; 
-
-
-            if (fontData.ContainsKey("ascender"))
-                font.Ascender = (uint)fontData["ascender"];
-            if (fontData.ContainsKey("ascenderOffset"))
-                font.AscenderOffset = (int)fontData["ascenderOffset"];
-
-            font.RangeStart = 0;
-            font.RangeEnd = 0;
-
-            foreach (JObject range in fontData["ranges"].Values<JObject>())
-            {
-                var rangeStart = (ushort)range["lower"];
-                var rangeEnd = (uint)range["upper"];
-                if (font.RangeStart > rangeStart)
-                    font.RangeStart = rangeStart;
-                if (font.RangeEnd > rangeEnd)
-                    font.RangeEnd = rangeEnd;
-            }
-
-            foreach (KeyValuePair<string, JToken> glyphMeta in (JObject)fontData["glyphs"])
-            {
-                var glyph = (JObject)glyphMeta.Value;
-                font.Glyphs.Add(new UndertaleFont.Glyph()
-                {
-                    Character = (ushort)glyph["character"],
-                    SourceX = (ushort)glyph["x"],
-                    SourceY = (ushort)glyph["y"],
-                    SourceWidth = (ushort)glyph["w"],
-                    SourceHeight = (ushort)glyph["h"],
-                    Shift = (short)glyph["shift"],
-                    Offset = (short)glyph["offset"],
-                });
-            }
-
-            List<UndertaleFont.Glyph> glyphs = font.Glyphs.ToList();
-
-            // I'm literally going to LINQ 100000 times
-            // and you can't stop me
-            foreach (JObject kerningPair in fontData["kerningPairs"].Values<JObject>())
-            {
-                var first = (ushort)kerningPair["first"];
-                var glyph = glyphs.Find(x => x.Character == first);
-                glyph.Kerning.Add(new UndertaleFont.Glyph.GlyphKerning()
-                {
-                    Other = (short)kerningPair["second"],
-                    Amount = (short)kerningPair["amount"],
-                });
-            }
-            // Sort glyphs like in UndertaleFontEditor to be safe
-            glyphs.Sort((x, y) => x.Character.CompareTo(y.Character));
-            font.Glyphs.Clear();
-
-            foreach (UndertaleFont.Glyph glyph in glyphs)
-                font.Glyphs.Add(glyph);
+            return fontYYInfoMap[fontName];
         }
+        var yy = new FileInfo(Path.Combine(FontPath, fontName+".yy"));
+        JObject fontData = null;
+        using (StreamReader file = File.OpenText((string)yy.FullName))
+        {
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                fontData = (JObject)JToken.ReadFrom(reader);
+            }
+        }
+        if (fontData == null) throw new Exception($"{FontPath}/{fontName}의 font data를 읽을 수 없습니다.");
+        fontYYInfoMap.Add(fontName, fontData);
+
+        return fontData;
+    }
+
+    /// <summary>
+    /// 기존 폰트명과 바꿀 폰트를 입력한다.
+    /// </summary>
+    /// <param name="originalFontName"></param>
+    /// <param name="fontName"></param>
+    private void ChangeFont(string originalFontName, string fontName)
+    {
+        var font = Data.Fonts.Where(x => x.Name.Content == originalFontName).FirstOrDefault();
+
+        var fontData = LoadFontData(fontName);
+        var texture = LoadFontTexture(fontName);
+
+        font.Texture = texture;
+        font.Glyphs.Clear();
+        font.DisplayName = Data.Strings.MakeString((string)fontData["fontName"]);
+        font.EmSize = (uint)fontData["size"];
+        font.Bold = (bool)fontData["bold"];
+        font.Italic = (bool)fontData["italic"];
+        font.Charset = (byte)fontData["charset"];
+        font.AntiAliasing = (byte)fontData["AntiAlias"];
+
+        if (fontData.ContainsKey("ascender"))
+            font.Ascender = (uint)fontData["ascender"];
+        if (fontData.ContainsKey("ascenderOffset"))
+            font.AscenderOffset = (int)fontData["ascenderOffset"];
+
+
+        font.RangeStart = 0;
+        font.RangeEnd = 0;
+
+
+
+
+        font.RangeStart = 0;
+        font.RangeEnd = 0;
+
+        foreach (JObject range in fontData["ranges"].Values<JObject>())
+        {
+            var rangeStart = (ushort)range["lower"];
+            var rangeEnd = (uint)range["upper"];
+            if (font.RangeStart > rangeStart)
+                font.RangeStart = rangeStart;
+            if (font.RangeEnd > rangeEnd)
+                font.RangeEnd = rangeEnd;
+        }
+
+        foreach (KeyValuePair<string, JToken> glyphMeta in (JObject)fontData["glyphs"])
+        {
+            var glyph = (JObject)glyphMeta.Value;
+            font.Glyphs.Add(new UndertaleFont.Glyph()
+            {
+                Character = (ushort)glyph["character"],
+                SourceX = (ushort)glyph["x"],
+                SourceY = (ushort)glyph["y"],
+                SourceWidth = (ushort)glyph["w"],
+                SourceHeight = (ushort)glyph["h"],
+                Shift = (short)glyph["shift"],
+                Offset = (short)glyph["offset"],
+            });
+        }
+
+        List<UndertaleFont.Glyph> glyphs = font.Glyphs.ToList();
+
+        // I'm literally going to LINQ 100000 times
+        // and you can't stop me
+        foreach (JObject kerningPair in fontData["kerningPairs"].Values<JObject>())
+        {
+            var first = (ushort)kerningPair["first"];
+            var glyph = glyphs.Find(x => x.Character == first);
+            glyph.Kerning.Add(new UndertaleFont.Glyph.GlyphKerning()
+            {
+                Other = (short)kerningPair["second"],
+                Amount = (short)kerningPair["amount"],
+            });
+        }
+        // Sort glyphs like in UndertaleFontEditor to be safe
+        glyphs.Sort((x, y) => x.Character.CompareTo(y.Character));
+        font.Glyphs.Clear();
+
+        foreach (UndertaleFont.Glyph glyph in glyphs)
+            font.Glyphs.Add(glyph);
+    }
+    public Patcher ApplyFont()
+    { 
+
+        foreach(var font in Data.Fonts) 
+            ChangeFont(font.Name.Content, "Font1"); 
 
 
         return this;
     }
     public Patcher End() => this;
-        
+
     public Patcher Save(string path)
     {
         Console.WriteLine("새로운 바이너리로 저장중..");
@@ -270,7 +303,7 @@ public class Patcher
         if (fi.Exists) fi.Delete();
 
         using FileStream fs = new FileInfo(path).OpenWrite();
-        UndertaleIO.Write(fs, Data); 
+        UndertaleIO.Write(fs, Data);
         return this;
     }
 
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -12,55 +12,62 @@ using static UndertaleModLib.Compiler.Compiler.AssemblyWriter;
 public static class Program
 {
     public static UndertaleData data;
-    public static string GameExePath => "./ZERO Sievert.exe";
-    public static string GamePath => "./";
-    public static string DataPath => "./data.win";
-    public static string GameArgs => "-game ./localized.win";
-    public static string LocalePath => "./localization/data.json";
-    public static string LocaleFontPath => "./localization/font";
+
+    /// <summary>
+    /// 게임 실행 경로
+    /// </summary>
+    public static string GameRootPath { get; set; }
+    /// <summary>
+    /// 데이터파일 경로
+    /// 임베드 데이터로드가 가능하려면 exe와 같은폴더에 있어야함
+    /// </summary>
+    public static string GameArgs => $"-game {MutatedDataPath}";
+
+
+    public static string GameExePath => $"{GameRootPath}/ZERO Sievert.exe";
+    public static string OriginalDataPath => $"{GameRootPath}./data.win";
+    public static string MutatedDataPath => $"{GameRootPath}/localized.win";
+    public static string LocalePath => $"./localization/data.json";
+    public static string LocaleFontDirectoryPath => $"./localization/font";
     static void Main(string[] args)
     {
-        while (true)
+        GameRootPath = "Game";
+        if (args.Length == 0)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("  [제로 시버트 한글패치 CLI] [v.001]");
-            Console.WriteLine("  오류수정/버그문의 : shlifedev@gmail.com ");
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" [제로 시버트 한글패치 CLI] [v.0.0.2]"); 
 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(" 아래의 숫자를 입력해주세요.");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" 1 -  입력시 게임 한글패치 진행 (30초-1분 정도 시간 소요)\n");
-            Console.WriteLine(" 2 -  입력시 한글패치 적용된 게임실행 (1번과정 완료시 생략가능)\n");
-            Console.WriteLine(" 0 -  [개발용] 모든 한글 출력\n");
-            Console.Write("숫자를 입력해주세요 :");
-            Console.ForegroundColor = ConsoleColor.White;
-            var input = Console.ReadLine();
-            Console.Clear();
-            if (input == "1")
-            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" 아래의 숫자를 입력해주세요.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(" 1 -  입력시 게임 한글패치 진행 (30초-1분 정도 시간 소요)\n");
+                Console.WriteLine(" 2 -  입력시 한글패치 적용된 게임실행 (1번과정 완료시 생략가능)\n");  
+                Console.ForegroundColor = ConsoleColor.White;
+                var input = Console.ReadLine();
                 Console.Clear();
-                var patcher = new Patcher(DataPath, LocalePath, LocaleFontPath);
-                patcher.ApplyTranslate();
-                patcher.ApplyFont();
-                patcher.Save("./localized.win");
-                Process.Start(GameExePath, GameArgs);
+                if (input == "1")
+                {
+                    Console.Clear();
+                    var patcher = new Patcher(OriginalDataPath, LocalePath, LocaleFontDirectoryPath);
+                    patcher.ApplyTranslate()
+                        .ApplyFont()
+                        .Save(MutatedDataPath);
+
+                    Process.Start(GameExePath, GameArgs);
+                }
+                if (input == "2")
+                {
+                    Process.Start(GameExePath, GameArgs);
+                    break;
+                } 
+                Console.Clear();
             }
-            if (input == "2")
-            {
-                Process.Start(GameExePath, GameArgs);
-                break;
-            }
-            if (input == "0")
-            {
-                var patcher = new Patcher(DataPath, LocalePath, LocaleFontPath);
-                patcher.ExportStrings("./localization/debug/en.json");
-                break;
-            }
-            Console.Clear();
         }
     }
 
- 
-     
-     
+
+
+
 }
