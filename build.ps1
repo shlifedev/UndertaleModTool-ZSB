87c1e9dd29520b6668864e1a4464d92cd915f934
shlifedev <shlifedev@gmail.com>
2023-01-07 22:20:35 +0900

    publish

--- build.ps1
+++ build.ps1
@@ -1,1 +1,2 @@
-dotnet publish Sample -c Release --self-contained -r "win10-x64" -p:PublishSingleFile=true
+dotnet publish Sample -c Release --self-contained -r "win10-x64" -p:PublishSingleFile=true -o "./build"
+xcopy dist build /e /h /k
