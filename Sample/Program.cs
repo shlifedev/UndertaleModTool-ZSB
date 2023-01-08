afa8cd0becab5bd2d2e86f4cb4ac422f5df5be67
shlifedev <shlifedev@gmail.com>
2023-01-08 16:37:44 +0900

    development mode

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Text.Json.Serialization;
 using System.IO;
-using static UndertaleModLib.Compiler.Compiler.AssemblyWriter;
+using static UndertaleModLib.Compiler.Compiler.AssemblyWriter; 
 
 public static class Program
 {
@@ -29,50 +29,39 @@ public static class Program
     public static string MutatedDataPath => $"{GameRootPath}/localized.win";
     public static string LocalePath => $"./localization/data.json";
     public static string LocaleFontDirectoryPath => $"./localization/font";
+
+    private static bool InDevelopment = false;
     static void Main(string[] args)
     {
         GameRootPath = ".";
 
+        if (InDevelopment)
+        {
+            Console.WriteLine("[개발자 모드] 구글시트로부터 최신 번역 파일 다운로드 중"); 
+            var task = Updator.GetUpdatedText();  
+            System.IO.File.WriteAllText(LocalePath, task.Result);
+
+            Console.WriteLine("게임을 실행하면 구글시트의 최신 작업내용이 반영되어 있어야 합니다.\n");
+            System.Threading.Thread.Sleep(500);
+        }  
+         
+
         while (!new FileInfo(GameExePath).Exists)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"[!] {GameRootPath}에서 ZERO Sievert.exe 를 찾을 수 없습니다. 게임 경로(폴더)를 직접 입력하세요");
+            Console.WriteLine($"[!] {GameRootPath}에서 ZERO Sievert.exe  를 찾을 수 없습니다. 이 파일은 제로시버트 설치경로에 포함시켜야 합니다. 게임 경로(폴더)를 직접 입력하세요");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("경로 입력:");
             Console.ForegroundColor = ConsoleColor.White;
             GameRootPath = Console.ReadLine();
         }
+        Console.Clear();
+        var patcher = new Patcher(OriginalDataPath, LocalePath, LocaleFontDirectoryPath);
+        patcher.ApplyTranslate()
+            .ApplyFont()
+            .Save(MutatedDataPath);
 
-        if (args.Length == 0)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(" [제로 시버트 한글패치 CLI] ");
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(" 아래의 숫자를 입력해주세요.");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" 1 -  입력시 게임 한글패치 진행 (30초-1분 정도 시간 소요)\n");
-            Console.WriteLine(" 2 -  입력시 한글패치 적용된 게임실행 (1번과정 완료시 생략가능)\n");
-            Console.WriteLine(" 3 -  입력시 패치가 적용되지 않은 게임실행\n");
-            Console.ForegroundColor = ConsoleColor.White;
-            var input = Console.ReadLine();
-            Console.Clear();
-            if (input == "1")
-            {
-                Console.Clear();
-                var patcher = new Patcher(OriginalDataPath, LocalePath, LocaleFontDirectoryPath);
-                patcher.ApplyTranslate()
-                    .ApplyFont()
-                    .Save(MutatedDataPath);
-
-                Process.Start(GameExePath, GameArgs);
-            }
-            if (input == "2")
-            {
-                Process.Start(GameExePath, GameArgs);
-            }
-            Console.Clear();
-        }
+        Process.Start(GameExePath, GameArgs);
     }
 
 
---
+++ Sample/Updator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Updator
+{
+    private static string jsonURL = "https://script.google.com/macros/s/AKfycbxRVBLdp0-fhQEcSaAH0ZzA7MpNSBXNJaIUFqRg22aL2LHW3tDCzLm9lAo5-GZYrZT39Q/exec";
+    public static async Task<string> GetUpdatedText()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(jsonURL);
+        request.Method = "GET"; 
+        var response = await request.GetResponseAsync();
+        string responseText = string.Empty;
+         
+        Stream respStream = response.GetResponseStream();
+
+        using (StreamReader sr = new StreamReader(respStream))
+        {
+            responseText = sr.ReadToEnd();
+        }
+
+        return responseText;
+    }
+}
