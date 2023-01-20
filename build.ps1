32e19d7eec1ce68030ad5be857ffa44972970420
shlifedev <shlifedev@gmail.com>
2023-01-20 19:42:01 +0900

    windows build script src change

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-dotnet publish Sample -c Release --self-contained -r "win10-x64" -p:PublishSingleFile=true -o "./build"
+dotnet publish Patcher -c Release --self-contained -r "win10-x64" -p:PublishSingleFile=true -o "./build"
 xcopy dist build /e /h /k
